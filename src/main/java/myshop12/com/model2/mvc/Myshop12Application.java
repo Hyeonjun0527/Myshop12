2272447e35a606bdbbe03167ac9278d5c5a245e1
최현준 <immio0@kangwon.ac.kr>
2024-05-01 21:20:46 +0900

    feature 2024.04.30 22:00 리액트 구매기능

--- src/main/java/myshop12/com/model2/mvc/Myshop12Application.java
+++ src/main/java/myshop12/com/model2/mvc/Myshop12Application.java
@@ -2,6 +2,8 @@ package myshop12.com.model2.mvc;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.context.annotation.FilterType;
@@ -21,10 +23,16 @@ import org.springframework.context.annotation.FilterType;
                 }),
 
         })//첫번째 scan할것, 두번째 @Configuration
-public class Myshop12Application {
+//서블릿 이니셜라이저는 web.xml 역할 수행할 수 있다.
+public class Myshop12Application extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(Myshop12Application.class, args);
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(Myshop12Application.class);
     }
 
 }
